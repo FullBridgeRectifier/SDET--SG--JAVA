4fd7019b453870327ac39ea6619eb42ef148ca8a
Victor Moran <VMoran@dealersocket.com>
2020-04-26 18:14:31 -0500

    Added separated tests

--- aliexpress-e2e/src/test/java/com/deviget/test/TSD_ValidateIphoneAd.java
+++ aliexpress-e2e/src/test/java/com/deviget/test/TSD_ValidateIphoneAd.java
@@ -28,7 +28,8 @@ public class TSD_ValidateIphoneAd extends TestDriver {
 			product.switchToNewTab();
 			product.validateStock();
 			
-		}catch(Exception e)
+		}
+		catch(Exception e)
 		{
 		    e.printStackTrace();
 		}
@@ -38,6 +39,65 @@ public class TSD_ValidateIphoneAd extends TestDriver {
 		
 	}
 	
+	
+///Following tests use the same methods but each step has its own test (this is useful for smoke testing, while the above option can be used for quick regression)
+	
+	@Test
+	public void ValidateLandingPageTest()
+	{
+		BSN_LandingPage landing = new BSN_LandingPage(driver);
+		
+		try 
+		{
+			landing.closePopupIfPresent();
+			landing.setSearch("iphone");
+			landing.clickSearch();			
+		}
+		catch(Exception e)
+		{
+		    e.printStackTrace();
+		}
+		assertEquals(landing.passed, true);
+		
+	}
+	
+	@Test
+	public void ValidateSearchResultsTest()
+	{
+		ValidateLandingPageTest();		
+		BSN_SearchResults search = new BSN_SearchResults(driver);	
+		try 
+		{
+			search.navigateToSecondPage();
+			search.clickItem();
+		}
+		catch(Exception e)
+		{
+		    e.printStackTrace();
+		}
+		assertEquals(search.passed, true);
+		
+	}
+	
+	@Test
+	public void ValidateProductPageTest()
+	{		
+		ValidateSearchResultsTest();
+		BSN_ProductPage product = new BSN_ProductPage(driver);	
+		try 
+		{
+			
+			product.switchToNewTab();
+			product.validateStock();
+			
+		}
+		catch(Exception e)
+		{
+		    e.printStackTrace();
+		}
+		assertEquals(product.passed, true);
+		
+	}
 	
 	
 	
