20216c11ff016c8084922a3c0c645c364b90402c
Victor Moran <VMoran@dealersocket.com>
2020-04-26 12:42:59 -0500

    added basic engine handler and methods to read the parameter file

--- aliexpress-e2e/src/main/java/com/deviget/controller/EngineFramework.java
+++ aliexpress-e2e/src/main/java/com/deviget/controller/EngineFramework.java
@@ -1,5 +1,95 @@
 package com.deviget.controller;
 
+import org.openqa.selenium.WebDriver;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class EngineFramework {
+	
+	public WebDriver driver;
+	public String baseURL;
+	public String browser;
+	public String defect;
+	public boolean passed=false;
+    String pathConfigFile;
+    int intDefaultTimeOut = 30;
+        
+    Properties prop = new Properties();
+    
+    public EngineFramework(WebDriver driver, String pathConfigFile)
+    {
+    	this.driver = driver;
+    	this.pathConfigFile = pathConfigFile;
+    }
+    
+    public void RunEngine()
+    {
+    	this.passed = false;
+    	this.defect = "Initializing engine";
+    	if(pathConfigFile.isEmpty())
+    	{
+    		this.defect = "Configuration file is empty, blank or null";
+    		System.out.println("pathconfigFile is empty");
+    		return;
+       	}
+    	
+    	if (!ReadParametersFile(pathConfigFile))
+        {
+       	    System.out.println("File was not found at: "+ pathConfigFile);
+            return;
+        }
+    	
+    }
+    
+	boolean ReadParametersFile(String pathConfigFile)
+	{
+		
+		System.out.println(".........................................ReadPropertiesFile");
+		if(verifyFileExist(pathConfigFile))
+		{
+			this.passed = false;
+			return false;
+		}
+		baseURL = prop.getProperty("baseUrl");
+		browser = prop.getProperty("browser").trim();
+		return true;	
+		
+	}
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Helper methods
+	private boolean verifyFileExist(String pathConfigFile) {
+		try{
+			prop.load(new FileInputStream(pathConfigFile));
+			this.passed = true;
+			return true;
+		}
+		catch(IOException ex){
+			this.passed = false;
+			this.defect = ex.getMessage();
+			return false;
+		}
+		
+	}
+    
+    
+    
+    
+    
 
 }
--- aliexpress-e2e/src/main/java/com/deviget/master/TestDriver.java
+++ aliexpress-e2e/src/main/java/com/deviget/master/TestDriver.java
@@ -1,5 +1,37 @@
 package com.deviget.master;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+
+import com.deviget.controller.*;
+
 public class TestDriver {
+	
+	protected String strPathConfigFile;
+	protected WebDriver driver;
+	protected EngineFramework engine;
+	
+	
+	
+	@BeforeClass
+    public void SetupEngine()
+    { 
+    	String parametersFile = new File(System.getProperty("user.dir")).getAbsolutePath() + "/Parameters.txt";
+        strPathConfigFile = parametersFile;        
+        Assert.assertEquals(StartEngine(strPathConfigFile), true);        
+    }	
+	
+	protected boolean StartEngine(String pathConfig)
+	{
+		engine = EngineFramework(driver, pathConfig);
+		
+	}
+	
+	
+	
+	
 
 }
